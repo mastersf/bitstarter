bce7fa431d05ef43a556263f706c9ec2b6fa12d2
mastersf <masterstanf2k@gmail.com>
2013-07-06 20:12:08 +0000

    web.js update

--- web.js
+++ web.js
@@ -8,9 +8,9 @@ fs.readFile('index.html', function (err, data) {
 	console.log(data);
 });
 
-app.get('/', function(request, response) {
+//app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-});
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
