985774f63bc6b8714547155de8816a39a0cab441
mastersf <masterstanf2k@gmail.com>
2013-07-06 22:22:16 +0000

    web.js update

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 var buffer = new Buffer(256);
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(buffer.toString('utf8', fs.readFileSync('index.html')));
+  //response.send(buffer.toString('utf8', fs.readFileSync('index.html')));
+  console.log(fs.readFileSync('index.html', 'utf8');
 });
 
 var port = process.env.PORT || 5000;
