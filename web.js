0106837497957d5d212beb0ea0c77ae7b3f50dbf
mastersf <masterstanf2k@gmail.com>
2013-07-06 20:26:50 +0000

    web.js update

--- web.js
+++ web.js
@@ -2,17 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var fs = require('fs');
+  var mybuff = new Buffer();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+	
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
-
-var fs = require('fs');
-fs.readFile('index.html', function (err, data) {
-        if (err) throw err;
-        console.log(data);
 });
 
